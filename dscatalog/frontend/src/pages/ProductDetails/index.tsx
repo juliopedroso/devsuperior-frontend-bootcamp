1a707465db1573c83fd5a154d30c08e82b2eeccc
Julio Cesar Pedroso <juliocp@weg.net>
2023-01-31 12:26:56 -0300

    ProductDetails - add HTML components

--- dscatalog/frontend/src/pages/ProductDetails/index.tsx
+++ dscatalog/frontend/src/pages/ProductDetails/index.tsx
@@ -1,6 +1,39 @@
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg'
+import ProductPrice from 'components/ProductPrice';
+
 const ProductDetails = () => {
     return (
-        <h1>Pagina ProductDetails</h1>
+
+        <div className="product-details-container">
+            <div className="product-details-card">
+                <div className="goback-container">
+                    <ArrowIcon />
+                    <h2>VOLTAR</h2>
+
+                </div>
+                <div className="row">
+                    <div className="col-xl-6">
+                        <div className="img-container">
+                            <img src='https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg' alt='Nome do Produto' />
+                        </div>
+                        <div className="name-price-container">
+                            <h1>Nome do Produto</h1>
+                            <ProductPrice price={12345.67} />
+                        </div>
+                    </div>
+                    <div className="col-xl-6">
+                        <div className="description-container">
+                            <h2>Descrição do Produto</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, ea.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
     )
 }
 
