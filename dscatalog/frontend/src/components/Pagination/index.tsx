4c1efe1dab8dfee2e0f46e22d7160cb9a02c9806
Julio Cesar Pedroso <juliocp@weg.net>
2023-02-03 12:24:58 -0300

    Pagination component styling

--- dscatalog/frontend/src/components/Pagination/index.tsx
+++ dscatalog/frontend/src/components/Pagination/index.tsx
@@ -1,16 +1,17 @@
+import './styles.css';
 import { ReactComponent as ArrowIcon } from "assets/images/arrow.svg";
 
 const Pagination = () => {
 
     return (
         <h1 className="pagination-container">
-            <ArrowIcon/>
-            <div className="pagination-item">1</div>
+            <ArrowIcon className='arrow-previous arrow-inactive'/>
+            <div className="pagination-item active">1</div>
             <div className="pagination-item">2</div>
             <div className="pagination-item">3</div>
             <div className="pagination-item">...</div>
             <div className="pagination-item">10</div>
-            <ArrowIcon/>
+            <ArrowIcon className='arrow-next arrow-active'/>
         </h1>
     );
 }
