7357ddb6379c2f2552d08fb9c69fdb2360f41057
Julio Cesar Pedroso <juliocp@weg.net>
2023-02-07 12:30:47 -0300

    Admin Navbar layout mobile

--- dscatalog/frontend/src/pages/Admin/index.tsx
+++ dscatalog/frontend/src/pages/Admin/index.tsx
@@ -1,6 +1,18 @@
+import './styles.css';
+import NavBar from "./NavBar";
 
-const Admin = ()=>{
-    return <h1>Página Admin</h1>
-}
+const Admin = () => {
+    return (
+
+        <div className="admin-container">
+            <NavBar/>
+            <div className="admin-content">
+
+                <h1>Conteudo</h1>
+
+            </div>
+        </div>
+    );
+};
 
 export default Admin;
