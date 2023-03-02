b27a48521a50e578f1d39b5d51c041df42a593fd
Julio Cesar Pedroso <juliopedroso@gmail.com>
2023-03-01 21:42:26 -0300

    Banner Styling

--- dscatalog/frontend/src/pages/Admin/Auth/index.tsx
+++ dscatalog/frontend/src/pages/Admin/Auth/index.tsx
@@ -1,3 +1,5 @@
+
+import './styles.css';
 import { ReactComponent as AuthImage } from 'assets/images/auth-image.svg';
 import { Route, Routes } from 'react-router-dom';
 
@@ -5,7 +7,7 @@ const Auth = () => {
     return (
         <div className="auth-container">
             <div className="auth-banner-container">
-                <h1>Divulge seus produtos no DS catalog</h1>
+                <h1>Divulge seus produtos no DS Catalog</h1>
                 <p>Faça parte do nosso catálogo de divulgação e aumente a venda dos seus produtos.</p>
                 <AuthImage />
             </div>
