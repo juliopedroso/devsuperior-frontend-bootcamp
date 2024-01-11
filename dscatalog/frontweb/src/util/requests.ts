8f44621a022790fe1d3e85353ca97d48bd1b3f71
Julio Cesar Pedroso <juliocp@weg.net>
2024-01-11 07:06:28 -0300

    Redirecting to login when 401 or 403

--- dscatalog/frontweb/src/util/requests.ts
+++ dscatalog/frontweb/src/util/requests.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig } from "axios";
 import QueryString from "qs";
-
+import history from './history'
 type LoginResponse = {
     access_token: string;
     token_type: string;
@@ -54,3 +54,21 @@ export const getAuthData = () => {
     const str = localStorage.getItem(tokenKey) ?? "{}";
     return JSON.parse(str) as LoginResponse;
 }
+
+// Add a request interceptor
+axios.interceptors.request.use(function (config) {
+    return config;
+}, function (error) {
+
+    return Promise.reject(error);
+});
+
+// Add a response interceptor
+axios.interceptors.response.use(function (response) {
+    return response;
+}, function (error) {
+    if (error.response.status === 401 || error.response.status === 403) {
+        history.push('/admin/auth')
+    }
+    return Promise.reject(error);
+});
