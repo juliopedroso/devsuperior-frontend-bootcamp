c50ce4eee219ec8b262682a82471fc918e549b2c
Julio Cesar Pedroso <juliopedroso@gmail.com>
2024-07-30 07:28:49 -0300

    Testing another hasAnyRoles scenario

--- dscatalog/frontweb/src/util/__tests__/auth.spec.ts
+++ dscatalog/frontweb/src/util/__tests__/auth.spec.ts
@@ -22,5 +22,17 @@ describe('hasAnyRoles tests', () => {
         //ASSERT
         expect(result).toEqual(true);
     });
+    test('should return false when user does not have given role', () => {
 
+        jest.spyOn(TokenModule,'getTokenData').mockReturnValue({
+            exp: 0,
+            user_name: '',
+            authorities: 'ROLE_OPERATOR',
+        });
+
+        //ACT
+        const result = hasAnyRoles(["ROLE_ADMIN"]);
+        //ASSERT
+        expect(result).toEqual(false);
+    });
 })
