19d9ed41a82e41f0bfb86fe85c22bac33c28f5a4
Julio Cesar Pedroso <juliopedroso@gmail.com>
2024-08-06 08:49:32 -0300

    MSW mock server, fixtures

--- dscatalog/frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
+++ dscatalog/frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -2,7 +2,22 @@ import { render, screen, waitFor } from "@testing-library/react";
 import Catalog from "..";
 import { Router } from "react-router-dom";
 import history from "util/history";
+import { server } from './fixtures';
 
+beforeAll(() => {
+    console.debug('Before all')
+    server.listen()
+})
+
+afterEach(() => {
+    console.debug('after each')
+    server.resetHandlers()
+})
+
+afterAll(() => {
+    console.debug('after all')
+    server.close()
+})
 
 test('should render Catalog with products', async () => {
 
@@ -15,13 +30,12 @@ test('should render Catalog with products', async () => {
             <Catalog />
         </Router>
     );
-    screen.debug();
 
     //ASSERT
     expect(screen.getByText("Catálogo de produtos")).toBeInTheDocument();
 
-    await waitFor(() => { 
-        expect(screen.getByText("Smart TV")).toBeInTheDocument() 
+    await waitFor(() => {
+        expect(screen.getByText("Smart TV")).toBeInTheDocument()
     });
 
 
