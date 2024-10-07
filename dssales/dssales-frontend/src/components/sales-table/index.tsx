8b88e49e45b8253d9bef3258801e262ce5cd6426
Julio Cesar Pedroso <juliocp@weg.net>
2024-10-07 08:11:53 -0300

    Add SalesTable component

---
+++ dssales/dssales-frontend/src/components/sales-table/index.tsx
@@ -0,0 +1,89 @@
+import './styles.css';
+
+function SalesTable() {
+  return (
+    <div className="sales-table-container base-card">
+      <h3 className="sales-table-title">Vendas recentes</h3>
+      <table className="sales-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Data</th>
+            <th>Gênero</th>
+            <th>Categoria</th>
+            <th>Loja</th>
+            <th>Forma de Pagamento</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>#1</td>
+            <td>07/11/2021</td>
+            <td>Feminino</td>
+            <td>Roupas e Acessorios</td>
+            <td>Uberlandia</td>
+            <td>Dinheiro</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+          <tr>
+            <td>#2</td>
+            <td>07/11/2021</td>
+            <td>Feminino</td>
+            <td>Roupas e Acessorios</td>
+            <td>Uberlandia</td>
+            <td>Dinheiro</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+          <tr>
+            <td>#3</td>
+            <td>07/11/2021</td>
+            <td>Feminino</td>
+            <td>Roupas e Acessorios</td>
+            <td>Uberlandia</td>
+            <td>Dinheiro</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+          <tr>
+            <td>#4</td>
+            <td>07/11/2021</td>
+            <td>Feminino</td>
+            <td>Roupas e Acessorios</td>
+            <td>Uberlandia</td>
+            <td>Dinheiro</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+          <tr>
+            <td>#5</td>
+            <td>07/11/2021</td>
+            <td>Feminino</td>
+            <td>Roupas e Acessorios</td>
+            <td>Uberlandia</td>
+            <td>Dinheiro</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+          <tr>
+            <td>#6</td>
+            <td>07/11/2021</td>
+            <td>Feminino</td>
+            <td>Roupas e Acessorios</td>
+            <td>Uberlandia</td>
+            <td>Dinheiro</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+          <tr>
+            <td>#7</td>
+            <td>07/11/2021</td>
+            <td>Feminino</td>
+            <td>Roupas e Acessorios</td>
+            <td>Uberlandia</td>
+            <td>Dinheiro</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default SalesTable;
